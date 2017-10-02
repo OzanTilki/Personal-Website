4d5abf5a5036b28a860b68541b89a1e2ac579f97
unknown <ozankeskin991@gmail.com>
2017-10-02 21:03:18 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import './Assets/css/default.min.css';
+import Header from './Components/headerComponent/header';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+      <Header />
+
+      </div>
+    );
+  }
+}
+
+export default App;
