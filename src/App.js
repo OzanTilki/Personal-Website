03e32e67cbc33cb056e5a0ddf6cccfec837bcda1
unknown <ozankeskin991@gmail.com>
2017-10-10 21:32:41 +0100

    Basic layout of a page with Components set-up

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 
-import './Assets/css/default.min.css';
+// import { render } from 'react-dom';
+//Components
 import Header from './Components/headerComponent/header';
-
+import Footer from './Components/footerComponent/footer';
+import HomePage from './Components/pages/homePage';
+//Includes
+import './Assets/css/default.min.css';
 class App extends Component {
   render() {
     return (
       <div className="App">
-
-      <Header />
-
+      <Header/>
+        <HomePage/>
+      <Footer/>
       </div>
     );
   }
--- src/Components/headerComponent/header.js
+++ src/Components/headerComponent/header.js
@@ -4,7 +4,25 @@ class Header extends Component {
   render() {
     return (
       <header>
-        
+        <div className="logo">
+        Logo
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <a href="Home">Home</a>
+            </li>
+            <li>
+              <a href="CV">Home</a>
+            </li>
+            <li>
+              <a href="Projects">Home</a>
+            </li>
+            <li>
+              <a href="Contact">Home</a>
+            </li>
+          </ul>
+        </nav>
       </header>
     );
   }
